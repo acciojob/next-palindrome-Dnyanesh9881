16be4a6b24d864d5e0b5e0a27a602fe1d9ab3928
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-02-09 15:22:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function nextPalindrome(num) {
-  //your JS code here.
+function isPalindrome(num) {
+    // Convert the number to a string and check if it reads the same backward
+    return num.toString() === num.toString().split('').reverse().join('');
+}
+
+function nextPalindrome(number) {
+    // Increment the number until a palindrome is found
+    while (true) {
+        number++;
+
+        // Check if the incremented number is a palindrome
+        if (isPalindrome(number)) {
+            return number;
+        }
+    }
 }
 
 const input = prompt("Enter a palindrome number");
